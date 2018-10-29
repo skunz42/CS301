f00190ae300a1f3ff5e43e0d7440011e5bd25ebd
skunz42 <skunz598@gmail.com>
2018-10-29 12:38:04 -0400

    Added some minor formatting fixes

--- MediaGroupMaker.py
+++ MediaGroupMaker.py
@@ -3,20 +3,23 @@ import csv
 with open('Event_Feedback.csv', mode='r') as csv_file:
     #consult your file for which group dictionary is which
     groups = [{},{},{},{},{},{}]
-    final = [ ["Hacking"],
-              ["Artificial Intelligence"],
-              ["Encryption"],
-              ["Piracy and Open Source"],
-              ["Social Media"],
-              ["Robotics"] ]
+    final = [ [],
+              [],
+              [],
+              [],
+              [],
+              [] ] #list of final MAP groups
     free = []
+    groupSize = 4 #this can be changed
     
-    data = [row for row in csv.reader(csv_file)] #csv in matrix form
+    data = [row for row in csv.reader(csv_file)] #reads csv into matrix form
     #Sets up groups
     for i, row in enumerate(data):
-        for col in range(2, len(row)):
+        for col in range(2, len(row)): #starts at 2 to ignore timestamp and name columns
             if i != 0:  #ignore first row
                 groups[col-2][data[i][1]] = data[i][col]
+            else: #inserts name of group into list to avoid confusion
+                final[col-2].append(data[i][col])
 
     #list of people not placed into groups
     for name in groups[0]:
@@ -28,7 +31,7 @@ with open('Event_Feedback.csv', mode='r') as csv_file:
         for i in range(len(groups)): #loop through topics
             for k,v in groups[i].items():
                 #if student has chosen this group and is not already in a group
-                if int(v) == target and len(final[i]) <= 4 and k in free:
+                if int(v) == target and len(final[i]) <= groupSize and k in free:
                     final[i].append(k)
                     free.remove(k) #student is placed in a group
         target+=1
