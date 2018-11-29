4e7d9a1af2ce3c47a7fd3f51afdfb68c19b8288a
skunz42 <skunz598@gmail.com>
2018-11-29 17:57:09 -0500

    Works for smaller amount of groups

--- MediaGroupMaker.py
+++ MediaGroupMaker.py
@@ -1,9 +1,23 @@
 import csv
 import sys
 
-def makeGroups(groups, final, free, groupSize):
+def removeGroup(groups, final, free, groupSize):
+    minLen = 10
+    minIndex = -1
+    for i in range(len(final)):
+        if len(final[i]) < minLen:
+            minLen = len(final[i])
+            minIndex = i
+
+    for s in final[minLen]:
+        free.append(s)
+
+    del final[minLen]
+    del groups[minLen]
+
+def makeGroups(groups, final, free, groupSize, numGroups):
     target = 0
-    while (target != 7): #loop through choice rankings
+    while (target != numGroups): #loop through choice rankings
         for i in range(len(groups)): #loop through topics
             for k,v in groups[i].items():
                 #if student has chosen this group and is not already in a group
@@ -11,7 +25,14 @@ def makeGroups(groups, final, free, groupSize):
                     final[i].append(k)
                     free.remove(k) #student is placed in a group
         target+=1
-
+    for f in final:
+        print(f)
+    print("-------------------------------")
+    #Find least populated group and add memebers to other groups
+    if numGroups > 6:
+        removeGroup(groups, final, free, groupSize)
+        makeGroups(groups, final, free, groupSize, numGroups-1)
+        
 def main():
     if len(sys.argv) < 2:
         print("Run as python (script name) (csv filename)")
@@ -25,7 +46,8 @@ def main():
                       [],
                       [] ] #list of final MAP groups
             free = []
-            groupSize = 4 #this can be changed 
+            groupSize = 4 #this can be changed
+            numGroups = 7
 
             data = [row for row in csv.reader(csv_file)] #reads csv into matrix form
             #Sets up groups
@@ -40,8 +62,5 @@ def main():
             for name in groups[0]:
                 free.append(name)
 
-            makeGroups(groups, final, free, groupSize)
-            print(groups)
-            #for f in final:
-             #   print(f)
+            makeGroups(groups, final, free, groupSize, numGroups)
 main()
